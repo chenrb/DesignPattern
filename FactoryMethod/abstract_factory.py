04fba743977f1f5bf5377a2604653982d6b215c5
crb <crbxyz@foxmail.com>
2018-09-05 00:03:40 +0800

    工厂模式：根据年龄不同选择不同的游戏

--- FactoryMethod/abstract_factory.py
+++ FactoryMethod/abstract_factory.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 
-class Frog:
+"""
+跟据年龄不同选择不同的游戏
+"""
 
+class Frog:
+    """青蛙游戏"""
     def __init__(self, name):
         self.name = name
 
@@ -9,5 +13,98 @@ class Frog:
         return self.name
 
     def interact_with(self, obstacle):
-        """"""
+        """描述青蛙与障碍物的交互"""
         print('{} the Frog encounters {} and {}!'.format(self, obstacle, obstacle.action()))
+
+
+class Bug:
+
+    def __str__(self):
+        return 'a bug'
+
+    def action(self):
+        return 'eat it'
+
+
+class FrogWorld:
+    """青蛙世界"""
+    def __init__(self, name):
+        print(self)
+        self.player_name = name
+
+    def __str__(self):
+        return '\n\n\t----Frog World-----'
+
+    def make_character(self):
+        return Frog(self.player_name)
+
+    def make_obstacle(self):
+        return Bug()
+
+
+class Wizard:
+    """勇士游戏"""
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+    def interact_with(self, obstacle):
+        print('{} the Wizard battles against {} and {}!'.format(self, obstacle, obstacle.action()))
+
+
+class Ork:
+    def __str__(self):
+        return 'an evil ork'
+
+    def action(self):
+        return 'kills it'
+
+
+class WizardWorld:
+    def __init__(self, name):
+        print(self)
+        self.player_name = name
+
+    def __str__(self):
+        return '\n\n\t------Wizard World-------'
+
+    def make_character(self):
+        return Wizard(self.player_name)
+
+    def make_obstacle(self):
+        return Ork()
+
+
+class GameEnvironment:
+    def __init__(self, factory):
+        self.hero = factory.make_character()
+        self.obstacle = factory.make_obstacle()
+
+    def play(self):
+        self.hero.interact_with(self.obstacle)
+
+
+def validate_age(name):
+    try:
+        age = input('Welcom {}, How old are you?\n'.format(name))
+        age  = int(age)
+    except ValueError as e:
+        print("Age {} is invalid, please try again.\n".format(age))
+        return (False, age)
+    return (True, age)
+
+
+def main():
+    name = input("Hello, What is your name?\n")
+    valid_input = False
+    while not valid_input:
+        valid_input, age = validate_age(name)
+    game = FrogWorld if age < 18 else WizardWorld
+    environment = GameEnvironment(game(name))
+    environment.play()
+
+
+if __name__ == '__main__':
+    main()
